4daccfda8e106e76773f9da5fff32c3d5e681c14
Gabriel Rojas <gabrielrojas_a@hotmail.com>
2019-01-19 00:56:02 -0600

    SQL injection security

--- songshelper/songshelper.go
+++ songshelper/songshelper.go
@@ -22,12 +22,13 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("sqlite3", "./jrdd.db")
 	checkErr(err)
 
+	param := "%" + r.URL.Query().Get("query") + "%"
 	query := fmt.Sprintf(
 		`SELECT songs.id, songs.artist, songs.song, songs.length, genres.name as genre 
 		FROM songs inner join genres on songs.genre = genres.id %v`,
 		searchQuery(r))
 
-	data := executeSongsQuery(query, db)
+	data := executeSongsQuery(query, db, param, param, param)
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, string(data))
 }
@@ -37,21 +38,22 @@ func IndexLength(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("sqlite3", "./jrdd.db")
 	checkErr(err)
 
+	queryString, params := buildQueryParams(r)
 	query := fmt.Sprintf(
 		`SELECT songs.id, songs.artist, songs.song, songs.length, genres.name as genre 
 		FROM songs inner join genres on songs.genre = genres.id 
 		%v 
 		ORDER BY songs.length DESC `,
-		buildQueryParams(r))
+		queryString)
 
-	data := executeSongsQuery(query, db)
+	data := executeSongsQuery(query, db, params...)
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, string(data))
 }
 
 // executeSongsQuery : executes the query and returns the list of songs in JSON format
-func executeSongsQuery(query string, db *sql.DB) []byte {
-	rows, err := db.Query(query)
+func executeSongsQuery(query string, db *sql.DB, args ...interface{}) []byte {
+	rows, err := db.Query(query, args...)
 	checkErr(err)
 	defer rows.Close()
 
@@ -72,29 +74,30 @@ func executeSongsQuery(query string, db *sql.DB) []byte {
 // searchQuery : returns the formatted string containing the 'where' clause of the index search query
 func searchQuery(r *http.Request) string {
 	if param := r.URL.Query().Get("query"); param != "" {
-		return fmt.Sprintf(
-			`where songs.artist like "%%%v%%" 
-			or songs.song like "%%%v%%"
-			or genres.name like "%%%v%%" `,
-			param, param, param)
+		return `where songs.artist like ? 
+			or songs.song like ?
+			or genres.name like ? `
 	}
 	return ""
 }
 
 // buildQueryParams : returns the formatted string containing the 'where' clause of the song length query
-func buildQueryParams(r *http.Request) string {
-	var params []string
+func buildQueryParams(r *http.Request) (string, []interface{}) {
+	var queryString []string
+	var params []interface{}
 
 	if max := r.URL.Query().Get("max"); max != "" {
-		params = append(params, fmt.Sprintf("songs.length < %v", max))
+		queryString = append(queryString, "songs.length < ?")
+		params = append(params, max)
 	}
 	if min := r.URL.Query().Get("min"); min != "" {
-		params = append(params, fmt.Sprintf("songs.length > %v", min))
+		queryString = append(queryString, "songs.length > ?")
+		params = append(params, min)
 	}
 	if len(params) > 0 {
-		return fmt.Sprintf("where %v", strings.Join(params, " and "))
+		return fmt.Sprintf("where %v", strings.Join(queryString, " and ")), params
 	}
-	return ""
+	return "", params
 }
 
 func checkErr(err error) {
